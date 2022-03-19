79d4b24252e713c887f42f336b33cd2d1c44760e
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2022-03-19 00:44:25 -0700

    pool page base

--- packages/synth-react/src/pages/PoolPage.js
+++ packages/synth-react/src/pages/PoolPage.js
@@ -1,9 +1,14 @@
 import pool_data from '../pool_data';
 import PoolBox from '../components/PoolBox';
+import Tvl from '../components/Tvl';
 export default function PoolPage(props) {
 
     return (
         <div className=" m-6">
+
+
+
+        <Tvl {...pool_data.stats}/>
         {/* LIST OF POOLS TO DEPOSIT INTO */}
         {pool_data.pools.map((pool, i) => {
             return ( <PoolBox key={i} {...pool} /> );
