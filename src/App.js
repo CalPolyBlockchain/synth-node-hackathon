f88cc00bc464122bd406d30e7c5369a32b9a5cbb
Max Mohammadi <maxmohammadi@gmail.com>
2022-03-20 06:26:17 -0700

    add more files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import React from "react";
+import PoolPage from "./pages/PoolPage";
+
+function App() {
+  // HOME PAGE
+  return (
+    <div className="p-5">
+      <PoolPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pool_data.js
@@ -0,0 +1,74 @@
+const pool_data = {
+    stats:
+    {
+        tvl_usd: 1048930.22,
+        tvl_change: 0.00,
+        tvl_change_percent: 0.00,
+        vol_24h: 10344.00,
+        total_interest: 2312,
+    },
+    current_pool: {
+        name: "Cusdt - CUSD",
+        symbol: "cUSD",
+        duration: "1d",
+        apr: "8.50",
+    },
+
+    previous_pools: [
+        {
+            name: "USDC (Optics V2",
+            avg_apr: "8.99",
+            interest_accumulated: "34.55",
+            duration: "1d",
+        },
+        {
+            name: "USDC (Optics V2",
+            avg_apr: "8.99",
+            interest_accumulated: "34.55",
+            duration: "1d",
+
+        },
+        {
+            name: "USDC (Optics V2",
+            avg_apr: "8.99",
+            interest_accumulated: "34.55",
+            duration: "1d",
+
+        },
+        {
+            name: "USDC (Optics V2",
+            avg_apr: "8.99",
+            interest_accumulated: "34.55",
+            duration: "1d",
+
+        }
+    ],
+
+    pools: [
+        {
+            name: "UST (Allbridge)",
+            symbols: ["cUSD", "UST"],
+            total_deposited: "$1,808,004",
+            weekly_volume: "$1,813",
+            apr: 0.279,
+            tvl: 738040.33,
+        },
+        {
+            name: "USDC (Optics V2)",
+            symbols: ["cUSD", "UST"],
+            total_deposited: "$1,808,004",
+            weekly_volume: "$1,813",
+            apr: 0.279,
+            tvl: 33330.00,
+        },
+        {
+            name: "DAI (Optics V2)",
+            symbols: ["cUSD", "DAI"],
+            total_deposited: "$2,880,004",
+            weekly_volume: "$1,333",
+            apr: 0.232,
+            tvl: 1000000.00,
+        }
+    ]
+}
+export default pool_data;
