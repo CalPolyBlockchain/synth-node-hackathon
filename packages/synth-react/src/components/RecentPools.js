dfb27b9b40934f909522c5a6057ae72514e7531d
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2022-03-20 04:33:30 -0700

    finish positons

--- packages/synth-react/src/components/RecentPools.js
+++ packages/synth-react/src/components/RecentPools.js
@@ -1,33 +1,33 @@
 export default function RecentPools(props) {
     return (
         <div>
-            <p> Recent Pools</p>
-            <div className="rounded-2xl p-3 m-3  text-white ">
+            <p className="text-center text-2xl text-white"> Recent Pools</p>
+            <div className="rounded-2xl pb-3 m-3 text-white ">
                 <div>
                     {/* create list of recent pools */}
                     {props.previous_pools.map((pool, i) => {
                         return (
-                            <div key={i} className="flex flex-center bg-white bg-opacity-40 m-2  p-3 rounded-2xl">
+                            <div key={i} className="flex flex-center bg-white bg-opacity-40 m-2 p-3 rounded-2xl">
                                 <div className="flex flex-col px-2">
                                     <p>{pool.name}</p>
                                     {/* <p>name</p> */}
                                     <img src={`./tokens/asset_${props.symbol}.png`} alt=''/>
                                 </div>
                                 <div className="flex flex-col px-2">
-                                    <p>{pool.duration}</p>
-                                    <p>duration</p>
+                                    <p className="text-pink">{pool.duration}</p>
+                                    <p className='label-text'>duration</p>
                                 </div>
                                 <div className="flex flex-col px-2">
-                                    <p>{pool.avg_apr}%</p>
-                                    <p>avg. apr</p>
+                                    <p className="text-green">{pool.avg_apr}%</p>
+                                    <p className='label-text'>avg. apr</p>
                                 </div>
                                 <div className="flex flex-col px-2">
-                                    <p>{pool.interest_accumulated}</p>
-                                    <p>interest accum.</p>
+                                    <p className="text-blue">{pool.interest_accumulated}</p>
+                                    <p className='label-text'>interest accum.</p>
                                 </div>
                             </div>
                         )
-                    })};
+                    })}
                 </div>
             </div>
         </div>
