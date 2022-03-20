4009c35d6c2b7072f5fe14e63cd0c37be7ac421e
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2022-03-20 04:37:12 -0700

    update tvl section

--- packages/synth-react/src/components/Tvl.js
+++ packages/synth-react/src/components/Tvl.js
@@ -1,7 +1,15 @@
 import StackedWrapper from '../components/StackedWrapper';
 import CurrentPool from './CurrentPool';
 import UserPositions from './UserPositions';
-import format_tvl from '../functions/formating';
+import RecentPools from './RecentPools';
+
+function format_tvl(tvl) {
+    if (tvl === null) {
+        return '-';
+    }
+    tvl = tvl.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return "$" + tvl;
+}
 
 export default function Tvl(props) {
     return(
@@ -9,15 +17,20 @@ export default function Tvl(props) {
         <StackedWrapper>
             <div className="m-6 text-white text-center">
                 <div className="text-4xl pb-4">
-                    <p>TVL: {()=> format_tvl(props.stats.tvl_usd)}</p>
+                    <p>TVL: {format_tvl(props.stats.tvl_usd)}</p>
                 </div>
-                <p className="text-2xl">24h vol. {props.stats.vol_24h}</p>
+                <p className="text-2xl">24h vol. {format_tvl(props.stats.vol_24h)}</p>
                 
                 <p> Total Interest Earned : ${props.stats.total_interest}</p>
                 </div>
                 <div className="grid grid-cols-2 content-center">
+                    <div className="p-2">
                     <CurrentPool {...props.current_pool}/>
+                    <RecentPools previous_pools={props.previous_pools}/>
+                    </div>
+                    <div className="p-2">
                     <UserPositions {...props.user}/>
+                    </div>
                 </div>
 
         </StackedWrapper>
