71ebd74c2fbc8fbc475cb6b3c3517095378a4272
Jack-kelly-22 <jacklaxjk@gmail.com>
2022-03-31 05:51:24 -0700

    update home and packages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,9 @@ import TeamSection from '../sections/TeamSection';
 // import { CryptoCards, Button } from 'web3uikit';
 import { home_page_text } from "../home_page_text";
 import GifCardSection from '../sections/GifCardSection';
+import OutlinedBox from "../components/OutlinedBox";
+import HeroSection3 from "../sections/HeroSection3";
+import PoolShowcase from "../components/PoolShowcase";
 
 function Home() {
   // HOME PAGE
@@ -19,10 +22,18 @@ function Home() {
       <div className="flex flex-row justify-center items-center ">
         <StackSection {...home_page_text.stack_section} />
       </div>
+      <div className="flex flex-col justify-center items-center">
+        {/* <OutlinedBox>
+          
+        </OutlinedBox> */}
+
+      </div>
       {/* Hero section 2 */}
       <div className="flex flex-row justify-center items-center ">
         <HeroSection2 {...home_page_text.hero_section2} />
       </div>
+      <HeroSection3 {...home_page_text.hero_section3}/>
+      <PoolShowcase apps = {home_page_text.apps}/>
       <GifCardSection {...home_page_text} />
         <TeamSection team={home_page_text.team} />
     </div>
