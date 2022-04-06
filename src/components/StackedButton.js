6254a3bf966666b1695a793256ad4f5209ffdc15
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2022-04-06 18:38:49 +0200

    update stacked button text

--- src/components/StackedButton.js
+++ src/components/StackedButton.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function StackedButton(props) {
     return (
-        <div className="text-4xl mt-12">
+        <div className="text-4xl mt-12 text-center">
             <div className="bg-yellow rounded-2xl stack-item  hover:bg-opacity-70 ">
                 <div className="bg-blue stack-item  hover:bg-opacity-70">
-                    <div className="bg-purple stack-item text-center  hover:bg-opacity-70">
-                        <p className="p-4 inline-block align-middle  hover:bg-opacity-70">{props.text}</p>
+                    <div className="bg-purple stack-item hover:bg-opacity-70">
+                        <p className="py-10">{props.text}</p>
                 
                 </div>
                 </div>
