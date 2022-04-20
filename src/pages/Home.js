21a923aed36f803d95cf6848fd8ea6b14280fa66
Jack-kelly-22 <jacklaxjk@gmail.com>
2022-04-19 22:11:26 -0700

    theme and r3emove

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,8 @@
-// import logo from './logo.svg';
 import React from "react";
 import HeroSection from "../sections/HeroSection";
 import HeroSection2 from "../sections/HeroSection2";
-// import OutlinedBox from "./components/OutlinedBox";
 import StackSection from '../sections/StackSection';
-import TeamSection from '../sections/TeamSection';
-// import { CryptoCards, Button } from 'web3uikit';
 import { home_page_text } from "../home_page_text";
-import GifCardSection from '../sections/GifCardSection';
-import OutlinedBox from "../components/OutlinedBox";
 import HeroSection3 from "../sections/HeroSection3";
 import PoolShowcase from "../components/PoolShowcase";
 
@@ -22,20 +16,12 @@ function Home() {
       <div className="flex flex-row justify-center items-center ">
         <StackSection {...home_page_text.stack_section} />
       </div>
-      <div className="flex flex-col justify-center items-center">
-        {/* <OutlinedBox>
-          
-        </OutlinedBox> */}
-
-      </div>
       {/* Hero section 2 */}
       <div className="flex flex-row justify-center items-center ">
         <HeroSection2 {...home_page_text.hero_section2} />
       </div>
       <HeroSection3 {...home_page_text.hero_section3}/>
       <PoolShowcase apps = {home_page_text.apps}/>
-      <GifCardSection {...home_page_text} />
-        <TeamSection team={home_page_text.team} />
     </div>
   );
 }
